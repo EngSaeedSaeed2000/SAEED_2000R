1cc836fb5295d08151c8dc97fad16b55ad023574
EngSaeedSaeed2000 <111233718+EngSaeedSaeed2000@users.noreply.github.com>
2023-06-23 15:12:58 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+$TOKEN = "6215463648:AAF5zATuZIZ6ZcxJ-Okv-kaBiwy0zYcrqIU";
+require "function.php";
+
+$inl=json_encode([
+"keyboard"=>[
+[["text"=>"A"]], 
+[["text"=>"B"]],
+[["text"=>"C"]],
+[["text"=>"D"]],
+    ]   
+    ]);
+    
+
+
+$web = SetWebhook("https://saeed2000r.000webhostapp.com/index.php");
+print_r($web);
+if($text == "/start"){
+SendMessage($chat_id,"أهلاً وسهلاً بكم"
+,null,false,$message_id,$inl);}
+if($text == "بحبك"){
+    SendMessage($chat_id,"أسف مرتبط"
+   ,null,false,$message_id );
+}
+if($text == "saeed"){
+    SendMessage($chat_id, "سعيد هو من برمج هذا البوت،
+    للتواصل مع سعيد 00963954947383"
+    ,null,false,$message_id);
+}
+if($text == "سعيد"){
+    SendMessage($chat_id, "سعيد هو من برمج هذا البوت،
+    للتواصل مع سعيد 00963954947383"
+   ,null,false, $message_id );
+}
+
+if($text == "A"){
+    SendDocument($chat_id,"https://drive.google.com/file/d/1HmUdAAz3ZZk8eXPChb90ROeMWMD-xA1s/view?usp=drivesdk.pdf");
+}
+if($text == "A"){
+    SendPhoto($chat_id,"https://images.app.goo.gl/ZjcyPvDpVGm9fNuH9");
+}
+
+?>
